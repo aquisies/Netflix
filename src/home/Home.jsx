643835f7e144f21c11667072fe91c8814b0ef59e
Alexander Mina <aquisiesalex@gmail.com>
2021-08-28 21:18:23 -0500

    first commit components  home elements

---
+++ src/home/Home.jsx
@@ -0,0 +1,204 @@
+import Navbar from "../components/navbar/Navbar";
+import "./home.scss";
+
+const Home = () => {
+	return (
+		<div className="home">
+			<Navbar />
+			<img
+				width="100%"
+				src="https://res.cloudinary.com/doovf5g5c/image/upload/v1623562798/Barbers/01_n67awf.png"
+				alt=""
+			/>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi,
+			libero ipsa beatae earum officia, a, ratione numquam consectetur sunt
+			deserunt ipsum distinctio necessitatibus nostrum impedit illum est.
+			Asperiores, repudiandae! Lorem ipsum dolor sit amet, consectetur
+			adipisicing elit. Quos sequi, libero ipsa beatae earum officia, a, ratione
+			numquam consectetur sunt deserunt ipsum distinctio necessitatibus nostrum
+			impedit illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae! Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Quos sequi, libero ipsa beatae earum
+			officia, a, ratione numquam consectetur sunt deserunt ipsum distinctio
+			necessitatibus nostrum impedit illum est. Asperiores, repudiandae! Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi, libero
+			ipsa beatae earum officia, a, ratione numquam consectetur sunt deserunt
+			ipsum distinctio necessitatibus nostrum impedit illum est. Asperiores,
+			repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			Quos sequi, libero ipsa beatae earum officia, a, ratione numquam
+			consectetur sunt deserunt ipsum distinctio necessitatibus nostrum impedit
+			illum est. Asperiores, repudiandae!
+		</div>
+	);
+};
+
+export default Home;
