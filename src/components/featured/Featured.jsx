178858b670b3d3a431c461092bf538fd4c755765
Alexander Mina <aquisiesalex@gmail.com>
2021-09-01 00:47:12 -0500

    Carrusel con limite de elementos y boton de desplazamiento inicial izquierdo oculto

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,56 @@
+import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+import "./featured.scss";
+
+export default function Featured({ type }) {
+	return (
+		<div className="featured">
+			{type && (
+				<div className="category">
+					<span>{type === "movie" ? "Movies" : "Series"}</span>
+					<select name="genre" id="genre">
+						<option>Genre</option>
+						<option value="adventure">Adventure</option>
+						<option value="comedy">Comedy</option>
+						<option value="crime">Crime</option>
+						<option value="fantasy">Fantasy</option>
+						<option value="historical">Historical</option>
+						<option value="horror">Horror</option>
+						<option value="romance">Romance</option>
+						<option value="sci-fi">Sci-fi</option>
+						<option value="thriller">Thriller</option>
+						<option value="western">Western</option>
+						<option value="animation">Animation</option>
+						<option value="drama">Drama</option>
+						<option value="documentary">Documentary</option>
+					</select>
+				</div>
+			)}
+			<img
+				src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/matrix-4-working-title-project-ice-cream-1568020586.jpg"
+				alt=""
+			/>
+			<div className="info">
+				<img
+					src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABRKrgH8goki6B50_fsipiwG8-efCu0w57iY4KsMiMOkV_xpwmCR1bwdO-qexN7jGsMEEVHu_udqNGVab2eGM-RxjYidvNBjuWkAE.png?r=df1"
+					alt=""
+				/>
+				<span className="desc">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque odio
+					eaque voluptatibus fugit, a quisquam sapiente culpa non est distinctio
+					eius natus quos dolor consectetur. Molestias dignissimos voluptatibus
+					repellat modi!
+				</span>
+				<div className="buttons">
+					<button className="play">
+						<PlayArrow />
+						<span>Play</span>
+					</button>
+					<button className="more">
+						<InfoOutlined />
+						<span>Info</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,9 @@
+import "./listItem.scss";
+
+export default function ListItem() {
+	return (
+		<div>
+			<div className="listItem">Item</div>
+		</div>
+	);
+}
